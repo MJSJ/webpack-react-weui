e95088fe2947095d388d7e18e6d524172dce3d54
yursile <289181450@qq.com>
2016-08-25 18:06:21 +0800

    ui

---
+++ src/js/page/test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import WeUI from 'react-weui';
+import 'weui';
+const {
+ 	Button,
+ 	Grids,
+ 	Page
+} = WeUI;
+
+
+
+
+export default class Test extends React.Component {
+  	render(){
+  		return(
+  			<Button>test</Button>
+  		)
+  	}
+};
+
+
